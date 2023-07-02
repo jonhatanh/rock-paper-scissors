775c9d3f5a95efd6133c61901e36a852dbc50417
jonhatanh <jonhatan.higuera@gmail.com>
2023-07-02 12:53:46 -0600

    Create game function and update playRound()

--- main.js
+++ main.js
@@ -8,20 +8,50 @@ function getComputerChoice() {
 
 function playRound(playerSelection = '', computerSelection) {
     playerSelection = playerSelection.toLowerCase()
-    let winnerMessage = '';
-    if (playerSelection === computerSelection) return `It's a draw! Both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`
-
-    if (playerSelection === 'rock') {
-        winnerMessage = computerSelection === 'paper' ? 'You Lose! Rock loses to Paper.' : 'You Win! Rock beats Scissors.';
-
-    } else if (playerSelection === 'paper') {
-        winnerMessage = computerSelection === 'scissors' ? 'You Lose! Paper loses to Scissors.' : 'You Win! Paper beats Rock.';
-        
-    } else if (playerSelection === 'scissors') {
-        winnerMessage = computerSelection === 'rock' ? 'You Lose! Scissors lose to Rock.' : 'You Win! Scissors beats Paper.';
-    } else {
-        winnerMessage = 'Unknown option'
+    const result = {winnerMessage: '', playerWon: undefined}
+    if (playerSelection === computerSelection) {
+        result.winnerMessage = `It's a draw! Both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+        return result;
     }
 
-    return winnerMessage;
+    if (playerSelection === 'rock') {
+        result.winnerMessage = computerSelection === 'paper' 
+        ? (result.playerWon = false, 'You Lose! Rock loses to Paper.') 
+        : (result.playerWon = true, 'You Win! Rock beats Scissors.');
+
+    } else if (playerSelection === 'paper') {
+        result.winnerMessage = computerSelection === 'scissors' 
+        ? (result.playerWon = false, 'You Lose! Paper loses to Scissors.' )
+        : (result.playerWon = true, 'You Win! Paper beats Rock.');
+        
+    } else if (playerSelection === 'scissors') {
+        result.winnerMessage = computerSelection === 'rock' 
+        ? (result.playerWon = false, 'You Lose! Scissors lose to Rock.') 
+        : (result.playerWon = true, 'You Win! Scissors beats Paper.');
+    } else {
+        result.winnerMessage = 'Unknown option'
+    }
+
+    return result;
+}
+
+function game() {
+    let round = 1;
+    const points = {player: 0, computer: 0};
+
+    while(round <= 5) {
+        let playerSelection = prompt('Select your option:')
+        let computerSelection = getComputerChoice();
+        console.log(`${playerSelection} vs ${computerSelection}`)
+        
+        const result = playRound(playerSelection, computerSelection)
+        console.log(result.winnerMessage)
+        if(result.playerWon) {
+            points.player++;
+        } else if (result.playerWon === false) {
+            points.computer++;
+        } 
+        round++;
+    }
+    console.log('Game Results: ',points);
 }
