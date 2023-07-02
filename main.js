2f9576ae06427b62436a7e25f821a792d1cecfec
jonhatanh <jonhatan.higuera@gmail.com>
2023-07-02 12:22:35 -0600

    Add playRound function

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ function getComputerChoice() {
 }
 
 
+function playRound(playerSelection = '', computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    let winnerMessage = '';
+    if (playerSelection === computerSelection) return `It's a draw! Both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`
+
+    if (playerSelection === 'rock') {
+        winnerMessage = computerSelection === 'paper' ? 'You Lose! Rock loses to Paper.' : 'You Win! Rock beats Scissors.';
+
+    } else if (playerSelection === 'paper') {
+        winnerMessage = computerSelection === 'scissors' ? 'You Lose! Paper loses to Scissors.' : 'You Win! Paper beats Rock.';
+        
+    } else if (playerSelection === 'scissors') {
+        winnerMessage = computerSelection === 'rock' ? 'You Lose! Scissors lose to Rock.' : 'You Win! Scissors beats Paper.';
+    } else {
+        winnerMessage = 'Unknown option'
+    }
+
+    return winnerMessage;
+}
